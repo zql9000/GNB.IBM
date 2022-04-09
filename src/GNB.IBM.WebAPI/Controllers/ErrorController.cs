1f01f4861d414d2598d1cf221049cdff6f9cdcb1
Lucas Ezequiel López <ezequiel2@gmail.com>
2022-04-09 08:07:55 -0300

    feat: Capture exceptions in production

---
+++ src/GNB.IBM.WebAPI/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GNB.IBM.WebAPI.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (context == null)
+            {
+                return NotFound();
+            }
+
+            return Problem(
+                instance: context.Path,
+                title: context.Error.Message
+                //detail: context.Error.StackTrace
+            );
+        }
+    }
+}
