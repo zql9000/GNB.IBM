9edf3a70d88dfc7c06209ac2cf4792377254c04e
Lucas Ezequiel López <ezequiel2@gmail.com>
2022-04-09 18:12:56 -0300

    feat: Add repository, HTTP handler and configure services

---
+++ src/GNB.IBM.Infrastructure/Repositories/ConversionRateRepository.cs
@@ -0,0 +1,45 @@
+﻿using GNB.IBM.Core.Configuration;
+using GNB.IBM.Core.Entities;
+using GNB.IBM.Core.Interfaces;
+using GNB.IBM.Core.Repositories;
+using GNB.IBM.Infrastructure.Repositories.Base;
+using Microsoft.Extensions.Options;
+
+namespace GNB.IBM.Infrastructure.Repositories
+{
+    public class ConversionRateRepository : Repository<ConversionRate>, IConversionRateRepository
+    {
+        protected readonly IHttpHandler<ConversionRate> _httpHandler;
+        protected readonly ExternalServicesSettings _settings;
+
+        public ConversionRateRepository(IHttpHandler<ConversionRate> httpHandler, IOptionsSnapshot<ExternalServicesSettings> settings)
+        {
+            _httpHandler = httpHandler;
+            _settings = settings.Value;
+        }
+
+        public async Task<IReadOnlyList<ConversionRate>> GetAllAsync()
+        {
+            string conversionRatesURI = _settings.ConversionRatesURI;
+            List<ConversionRate>? objects = null;
+
+            try
+            {
+                objects = await _httpHandler.GetAsync(conversionRatesURI);
+            }
+            catch (Exception)
+            {
+                // Get data from database
+                objects = new List<ConversionRate>();
+            }
+            
+            if (objects is null)
+            {
+                // Get data from database
+                objects = new List<ConversionRate>();
+            }
+
+            return objects;
+        }
+    }
+}
--- src/GNB.IBM.WebAPI/Program.cs
+++ src/GNB.IBM.WebAPI/Program.cs
@@ -1,13 +1,16 @@
+using GNB.IBM.Application.Interfaces;
+using GNB.IBM.Application.Services;
+using GNB.IBM.Core.Configuration;
+using GNB.IBM.Core.Repositories;
+using GNB.IBM.Core.Repositories.Base;
+using GNB.IBM.Infrastructure.Repositories;
+using GNB.IBM.Infrastructure.Repositories.Base;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .WriteTo.Console()
-    .WriteTo.File("serilog.txt", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
+ConfigureCustomServices(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -34,3 +37,28 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureCustomServices(IServiceCollection services, IConfiguration configuration)
+{
+    // Add Core Layer
+    services.Configure<ExternalServicesSettings>(
+        configuration.GetSection(ExternalServicesSettings.ExternalServices)
+    );
+
+    // Add Infrastructure Layer
+    services.AddHttpClient();
+    services.AddSingleton(typeof(GNB.IBM.Core.Interfaces.IHttpHandler<>), typeof(GNB.IBM.Infrastructure.Services.HttpHandler<>));
+    services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+    services.AddScoped<IConversionRateRepository, ConversionRateRepository>();
+
+    // Add Application Layer
+    services.AddScoped<IConversionRateService, ConversionRateService>();
+
+    // Add WebAPI Layer
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("serilog.txt", rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+}
