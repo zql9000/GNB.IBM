413d19ebccd7b8c2491d0c146c54d8dedf7f5807
Lucas Ezequiel López <ezequiel2@gmail.com>
2022-04-10 15:42:44 -0300

    feat: Get data from data base when services returns error

--- src/GNB.IBM.Infrastructure/Repositories/ProductTransactionRepository.cs
+++ src/GNB.IBM.Infrastructure/Repositories/ProductTransactionRepository.cs
@@ -2,6 +2,7 @@
 using GNB.IBM.Core.Entities;
 using GNB.IBM.Core.Interfaces;
 using GNB.IBM.Core.Repositories;
+using GNB.IBM.Infrastructure.Data;
 using GNB.IBM.Infrastructure.Repositories.Base;
 using Microsoft.Extensions.Options;
 
@@ -9,63 +10,28 @@ namespace GNB.IBM.Infrastructure.Repositories
 {
     public class ProductTransactionRepository : Repository<ProductTransaction>, IProductTransactionRepository
     {
-        protected readonly IHttpHandler<ProductTransaction> _httpHandler;
         protected readonly ExternalServicesSettings _settings;
 
-        public ProductTransactionRepository(IHttpHandler<ProductTransaction> httpHandler, IOptionsSnapshot<ExternalServicesSettings> settings)
+        public ProductTransactionRepository(
+            IHttpHandler<ProductTransaction> httpHandler, 
+            IOptionsSnapshot<ExternalServicesSettings> settings,
+            DatabaseContext context) : base(context, httpHandler)
         {
-            _httpHandler = httpHandler;
             _settings = settings.Value;
         }
 
         public async Task<IEnumerable<ProductTransaction>> GetProductTransactionListAsync()
         {
             string productTransactionsURI = _settings.ProductTransactionsURI;
-            List<ProductTransaction>? productTransactions = null;
-
-            try
-            {
-                productTransactions = await _httpHandler.GetAsync(productTransactionsURI);
-            }
-            catch (Exception)
-            {
-                // Get data from database
-                productTransactions = new List<ProductTransaction>();
-            }
-
-            if (productTransactions is null)
-            {
-                // Get data from database
-                productTransactions = new List<ProductTransaction>();
-            }
-
-            return productTransactions;
+            return await GetAllAsync(productTransactionsURI);
         }
 
         public async Task<IEnumerable<ProductTransaction>> GetProductTransactionListBySkuAsync(string sku)
         {
             string productTransactionsURI = _settings.ProductTransactionsURI;
-            List<ProductTransaction>? productTransactions = null;
+            var productTransactions = await GetAllAsync(productTransactionsURI);
 
-            try
-            {
-                productTransactions = await _httpHandler.GetAsync(productTransactionsURI)
-                    ?? new List<ProductTransaction>();
-                productTransactions = productTransactions.Where(t => t.SKU == sku).ToList();
-            }
-            catch (Exception)
-            {
-                // Get data from database
-                productTransactions = new List<ProductTransaction>();
-            }
-
-            if (productTransactions is null)
-            {
-                // Get data from database
-                productTransactions = new List<ProductTransaction>();
-            }
-
-            return productTransactions;
+            return productTransactions.Where(t => t.SKU == sku).ToList();
         }
     }
 }
